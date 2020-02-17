f35bc3fa3644767df9a9de7612d17ece6ee922f4
lihonglin <lihongllin@qq.com>
2020-02-17 21:40:57 +0800

    feat: define ImageEditor props

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <ImageEditor />
+    <ImageEditor v-model="img.url" />
   </div>
 </template>
 
@@ -11,6 +11,14 @@ export default {
   name: 'App',
   components: {
     ImageEditor,
+  },
+
+  data() {
+    return {
+      img: {
+        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1581956797668&di=2a5f1a3cf4810c8ec54130f9c148dbb7&imgtype=0&src=http%3A%2F%2Ffiles.eduuu.com%2Fimg%2F2012%2F04%2F13%2F104001_4f8792019aafd.jpg',
+      },
+    }
   },
 }
 </script>
--- src/components/ImageEditor.vue
+++ src/components/ImageEditor.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="image-editor">
-    haha
+    <div class="img-container">
+      <img :src="value" />
+    </div>
+    <canvas>
+      你的浏览器不支持canvas
+    </canvas>
+    <svg></svg>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ImageEditor',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
 }
 </script>
+
